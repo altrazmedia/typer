760ed5dfe1f9fceca7e9838f325bbfff6e956744
Bogusz Napierała <bognapierala@gmail.com>
2022-11-11 21:29:44 +0100

    Install react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Router } from "src/features/navigation";
 import { AuthContextProvider } from "./features/auth/AuthContext";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <AuthContextProvider>
-      <Router />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 };
 
