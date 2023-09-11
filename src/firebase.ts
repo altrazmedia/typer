e626f9659e0b88a83a0b60b4e7d035303571d4d1
Bogusz Napierała <bognapierala@gmail.com>
2023-09-11 22:45:51 +0200

    remove log

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { collection, CollectionReference, DocumentData, getFirestore } from "firebase/firestore";
 
-console.log(import.meta.env);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
