7e92959ff1ea41b1acece5d2482f75ed0ff74b5d
Bogusz Napierała <bognapierala@gmail.com>
2024-06-18 00:12:11 +0200

    add default options for queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextProvider } from "src/features/auth/contexts";
 import { Router } from "src/features/navigation";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5,
+    }
+  }
+});
 
 const App = () => {
   return (
