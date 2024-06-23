256722263b99467c27f39aa2a9c4963b2f48e75b
Bogusz Napierała <bognapierala@gmail.com>
2024-06-23 23:46:37 +0200

    8 Add toast notifications (#9)
    * handle notifications
    
    * add notifications component
    
    * display notifications after adding or editing predictions and games

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { AuthContextProvider } from "src/features/auth/contexts";
 import { Router } from "src/features/navigation";
+import { NotificationsProvider } from "src/features/notifications/context/NotificationsContext";
+import { ToastNotifications } from "src/features/notifications/components/ToastsNotifications";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -10,17 +12,20 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       refetchOnMount: false,
       staleTime: 1000 * 60 * 5,
-    }
-  }
+    },
+  },
 });
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <Router />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </AuthContextProvider>
+      <NotificationsProvider>
+        <AuthContextProvider>
+          <ToastNotifications />
+          <Router />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </AuthContextProvider>
+      </NotificationsProvider>
     </QueryClientProvider>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'toast-in': {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        'toast-out': {
+          '0%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '0', transform: 'translateY(20px)',  marginTop: '0', height: '0' },
+        },
+      },
+      animation: {
+        'toast-in': 'toast-in 0.5s ease-out forwards',
+        'toast-out': 'toast-out 0.5s ease-in forwards',
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
